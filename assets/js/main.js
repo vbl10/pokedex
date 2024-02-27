e2b3d12b7f6226c4d89f1b62d1817b1543e11b0c
Vinicius Baumann Ladosky <vinicius.ladosky@gmail.com>
2024-02-27 17:16:43 -0300

    Convertendo modelo pokeApi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,25 @@
-const offset = 10;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 const pokemonsListElement = document.getElementById("pokemonsList");
 
-function makeHtmlPokemonTypeList(pokemonStruct) {
-  return pokemonStruct.types.map((elmnt) => {
-    return `<li class="tipo">${elmnt.type.name}</li>`;
-  }).join("");
+function makeHtmlPokemonTypeList(pokemonModel) {
+  return pokemonModel.types
+    .map((type) => {
+      return `<li class="tipo">${type}</li>`;
+    })
+    .join("");
 }
 
-function makeHtmlPokemonCard(pokemonStruct) {
-  let name = pokemonStruct.name;
-  name = name[0].toUpperCase() + name.slice(1);
+function makeHtmlPokemonCard(pokemonModel) {
   return `
     <li class="pokemon">
-      <span class="numero">#${pokemonStruct.order}</span>
-      <span class="nome">${name}</span>
+      <span class="numero">#${pokemonModel.number}</span>
+      <span class="nome">${pokemonModel.name}</span>
       <div class="detalhes">
         <ol class="tipos">
-          ${makeHtmlPokemonTypeList(pokemonStruct)}
+          ${makeHtmlPokemonTypeList(pokemonModel)}
         </ol>
         <img
-          src="${pokemonStruct.sprites.other.dream_world.front_default}"
-          alt="${name}"
+          src="${pokemonModel.sprite}"
+          alt="${pokemonModel.name}"
           srcset=""
         />
       </div>
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,17 +1,34 @@
-
+class PokemonModel {
+  constructor(pokeApiModel) {
+    this.number = pokeApiModel.order;
+    this.name =
+      pokeApiModel.name.slice(0, 1).toUpperCase() + pokeApiModel.name.slice(1);
+    this.types = pokeApiModel.types.map((elmnt) => elmnt.type.name);
+    this.mainType = this.types[0];
+    this.sprite = pokeApiModel.sprites.other.dream_world.front_default;
+  }
+  number;
+  name;
+  mainType;
+  types = [];
+  sprite;
+}
 class pokeApi {
   static getPokemons(offset = 0, limit = 10) {
-
     const getPokemonDetailsPromise = function (elmnt) {
-      return fetch(elmnt.url).then((pokemonDetailsJson) => pokemonDetailsJson.json());
-    }
+      return fetch(elmnt.url)
+        .then((pokemonDetailsJson) => pokemonDetailsJson.json())
+        .then((pokeApiModel) => new PokemonModel(pokeApiModel));
+    };
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
       .then((response) => response.json()) //.json() is asynchronous because until here only the response header has been received
       .then((responseStruct) => responseStruct.results)
       .then((pokemonList) => pokemonList.map(getPokemonDetailsPromise))
-      .then((pokemonDetailsPromiseList) => Promise.all(pokemonDetailsPromiseList)) //waits for all promises to fulfill and returns pokemonDetailsList
+      .then((pokemonDetailsPromiseList) =>
+        Promise.all(pokemonDetailsPromiseList)
+      ) //waits for all promises to fulfill and returns pokemonDetailsList
       .catch((error) => console.log(error));
   }
 }
